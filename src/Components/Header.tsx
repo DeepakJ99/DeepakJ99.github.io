8e2a1daf242ecdb539f50d06d973f298b2251a91
Deepak Jakhu <djakhu99@gmail.com>
2025-08-12 21:09:48 +0530

    Bigger icons

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,9 @@
-import { FaHome, FaMoon, FaReadme, FaSun, FaUser } from "react-icons/fa";
+import { FaHome, FaMoon, FaSun} from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Breadcrumbs from "./Breadcrumb";
-
+import { FaUser } from "react-icons/fa6";
+import { IoReader } from "react-icons/io5";   
 export default function Header() {
     const [darkMode, setDarkMode] = useState(false);
 
@@ -13,24 +14,24 @@ export default function Header() {
     return (
         <header className="w-full border-b border-gray-200 px-4 sm:px-6 py-3 sm:py-4 bg-stone-50 dark:bg-zinc-800 sticky top-0 z-50 dark:border-zinc-700">
             <div className="flex flex-col items-end space-y-2">
-                <nav className="flex space-x-4 sm:space-x-6 text-sm font-medium text-gray-700 dark:text-white items-center text-lg sm:text-sm">
+                <nav className="flex text-sm font-medium text-gray-700 dark:text-white items-center text-lg sm:text-sm">
                     <NavLink
                         to="/"
                         className={({ isActive }) =>
-                            "transform transition-transform duration-100 hover:scale-120 " +
+                            "transform transition-transform duration-100 hover:scale-120 p-4 " +
                             (isActive ? "text-blue-500" : "")
                         }
                     >
-                        <FaHome />
+                        <FaHome className="text-xl sm:text-lg"/>
                     </NavLink>
                     <NavLink
                         to="/articles"
                         className={({ isActive }) =>
-                            "transform transition-transform duration-100 hover:scale-120 " +
+                            "transform transition-transform duration-100 hover:scale-120 p-4 " +
                             (isActive ? "text-blue-500" : "")
                         }
                     >
-                        <FaReadme />
+                        <IoReader className="text-xl sm:text-lg"/>
                     </NavLink>
                     {/* <NavLink
                         to="/fun-projects"
@@ -44,13 +45,13 @@ export default function Header() {
                     <NavLink
                         to="/profile"
                         className={({ isActive }) =>
-                            "transform transition-transform duration-100 hover:scale-120 " +
+                            "transform transition-transform duration-100 hover:scale-120 p-4 " +
                             (isActive ? "text-blue-500" : "")
                         }
                     >
-                        <FaUser />
+                        <FaUser className="text-xl sm:text-lg" />
                     </NavLink>
-                    <button onClick={() => setDarkMode(!darkMode)} className="inline-flex items-center transform transition-transform duration-100 hover:scale-120">
+                    <button onClick={() => setDarkMode(!darkMode)} className="inline-flex items-center transform transition-transform duration-100 text-xl sm:text-lg hover:scale-120 cursor-pointer p-4 ">
                         {darkMode ? <FaSun /> : <FaMoon />}
                     </button>
                 </nav>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -28,21 +28,24 @@ export default function LandingPage() {
     navigate(`articles/${articleResponse?.slug}`)
   }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-stone-50 dark:bg-zinc-800 dark:text-stone-50 text-zinc-800 px-4 sm:px-6">
-    {articleResponse ? (
-      <>
-        <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-center max-w-xl sm:max-w-2xl">
-          {articleResponse.title}
-        </h1>
-        <button
-          className="bg-black text-white mt-6 sm:mt-10 px-4 sm:px-6 py-2 rounded-full text-sm font-medium 
+   <div
+  className="flex flex-col items-center justify-center bg-stone-50 dark:bg-zinc-800 dark:text-stone-50 text-zinc-800 px-4 sm:px-6"
+  style={{ minHeight: "calc(100vh - 200px)" }}
+>
+  {articleResponse ? (
+    <>
+      <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-center max-w-xl sm:max-w-2xl">
+        {articleResponse.title}
+      </h1>
+      <button
+        className="bg-black text-white mt-6 sm:mt-10 px-4 sm:px-6 py-2 rounded-full text-sm font-medium 
                     transition-transform transform hover:scale-105 hover:shadow-lg 
                     hover:bg-gray-800 duration-200"
-          onClick={handleClick}
-        >
-          Read More
-        </button>
-      </>
+        onClick={handleClick}
+      >
+        Read More
+      </button>
+    </>
     ) : (
       <Loader />
     )}
